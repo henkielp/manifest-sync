363beef608c1f2502e4cc95fb34cb6dfd45aa989
henkielp <64457536+henkielp@users.noreply.github.com>
2026-07-07 17:50:23 -0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.manifestscanner"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.7.0"
+        versionCode = 2
+        versionName = "0.7.1"
     }
 
 
